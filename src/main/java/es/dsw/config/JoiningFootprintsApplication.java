4e5df0d7f8214601e986efdc34ba65c115cbd190
‘kvjs75’ <kvjs75@gmail.com>
2024-01-19 17:18:31 +0100

    inicio de proyecto

---
+++ src/main/java/es/dsw/config/JoiningFootprintsApplication.java
@@ -0,0 +1,13 @@
+package es.dsw.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoiningFootprintsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoiningFootprintsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/dsw/config/JoiningFootprintsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dsw.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoiningFootprintsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
